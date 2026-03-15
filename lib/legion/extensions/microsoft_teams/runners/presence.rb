7bbe560b7819f0d3921ec78960a9cd7bb3706721
Esity <matthewdiverson@gmail.com>
2026-03-15 18:20:50 -0500

    fix presence runner to use graph_connection pattern and helpers::lex guard

--- lib/legion/extensions/microsoft_teams/runners/presence.rb
+++ lib/legion/extensions/microsoft_teams/runners/presence.rb
@@ -1,20 +1,29 @@
 # frozen_string_literal: true
 
+require 'legion/extensions/microsoft_teams/helpers/client'
+
 module Legion
   module Extensions
     module MicrosoftTeams
       module Runners
         module Presence
-          def get_presence(user_id:)
-            response = client.get("/users/#{user_id}/presence")
+          include Legion::Extensions::MicrosoftTeams::Helpers::Client
+
+          def get_presence(user_id:, **)
+            conn = graph_connection(**)
+            response = conn.get("/users/#{user_id}/presence")
+            body = response.body || {}
             {
-              availability: response['availability'] || response[:availability],
-              activity:     response['activity'] || response[:activity],
+              availability: body['availability'],
+              activity:     body['activity'],
               fetched_at:   Time.now.utc
             }
           rescue StandardError => e
             { availability: 'Offline', activity: 'OffWork', error: e.message, fetched_at: Time.now.utc }
           end
+
+          include Legion::Extensions::Helpers::Lex if Legion::Extensions.const_defined?(:Helpers) &&
+                                                      Legion::Extensions::Helpers.const_defined?(:Lex)
         end
       end
     end
